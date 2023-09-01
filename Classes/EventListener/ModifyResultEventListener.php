e28b462a4ee4c7ee1a415d2c7bc4901c23b5c9c8
Georg Ringer <georg.ringer@gmail.com>
2023-09-01 09:21:01 +0200

    [TASK] Restructure code

--- Classes/EventListener/ModifyResultEventListener.php
+++ Classes/EventListener/ModifyResultEventListener.php
@@ -9,13 +9,13 @@ use StudioMitte\LiveSearchExtended\Event\ModifyRowEvent;
 use TYPO3\CMS\Backend\Routing\UriBuilder;
 use TYPO3\CMS\Backend\Search\Event\ModifyResultItemInLiveSearchEvent;
 use TYPO3\CMS\Backend\Search\LiveSearch\DatabaseRecordProvider;
+use TYPO3\CMS\Backend\Search\LiveSearch\ResultItem;
 use TYPO3\CMS\Backend\Search\LiveSearch\ResultItemAction;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\Imaging\Icon;
 use TYPO3\CMS\Core\Imaging\IconFactory;
 use TYPO3\CMS\Core\Localization\LanguageService;
 use TYPO3\CMS\Core\Localization\LanguageServiceFactory;
-use TYPO3\CMS\Extbase\Event\Mvc\BeforeActionCallEvent;
 
 final class ModifyResultEventListener
 {
@@ -38,7 +38,10 @@ final class ModifyResultEventListener
             return;
         }
 
-        $table = $resultItem->getExtraData()['table'] ?? null;
+        $table = $resultItem->getExtraData()['table'] ?? '';
+        if (!$table) {
+            return;
+        }
         $searchConfiguration = Table::createFromTCA($table);
 
         $row = $resultItem->getInternalData()['row'] ?? null;
@@ -50,43 +53,58 @@ final class ModifyResultEventListener
         $row = $rowEvent->getRow();
 
         if ($searchConfiguration && $searchConfiguration->isValid()) {
-            foreach ($searchConfiguration->getFields() as $field) {
-                if (!isset($row[$field->field])) {
-                    continue;
-                }
-
-                if (isset($GLOBALS['TCA'][$table]['columns'][$field->field])) {
-                    $label = $this->languageService->sL(BackendUtility::getItemLabel($table, $field->field));
-                    $content = BackendUtility::getProcessedValue($table, $field->field, $row[$field->field], 0, false, false, $row['uid']);
-                } else {
-                    $label = $this->languageService->sL($field->getLabel());
-                    $content = $row[$field->field];
-                }
-                if (!$content && $field->isSkipIfEmpty()) {
-                    continue;
-                }
-                $text = $field->isPrefixLabel() ? sprintf('%s: %s', $label, $content) : $content;
-                $action = (new ResultItemAction($table . '_' . $field->field))
-                    ->setLabel($text)
-                    ->setIcon($field->icon ? $this->iconFactory->getIcon($field->icon, Icon::SIZE_SMALL) : null);
-                $resultItem->addAction($action);
-            }
-
-            $notesField = $GLOBALS['TCA'][$table]['ctrl']['descriptionColumn'] ?? null;
-            if ($notesField && ($row[$notesField] ?? false) && $searchConfiguration->getUseNotesField()) {
-                $content = BackendUtility::getProcessedValue($table, $notesField, $row[$notesField]);
-
-                $action = (new ResultItemAction($table . '_descriptionColumn'))
-                    ->setLabel($content)
-                    ->setIcon($this->iconFactory->getIcon('actions-notebook', Icon::SIZE_SMALL));
-                $resultItem->addAction($action);
-            }
-
-            // add ID to title
-            $currentTitle = $resultItem->jsonSerialize()['itemTitle'] ?? false;
-            if ($currentTitle) {
-                $resultItem->setItemTitle(sprintf('%s [%s]', $currentTitle, $row['uid']));
-            }
+            $this->addFieldsToResult($resultItem, $searchConfiguration, $table, $row);
+            $this->addNotesToResult($resultItem, $searchConfiguration, $table, $row);
+            $this->appendIdToTitle($resultItem, $row['uid']);
         }
     }
+
+    protected function addFieldsToResult(ResultItem $resultItem, Table $searchConfiguration, string $table, array $row): void
+    {
+        foreach ($searchConfiguration->getFields() as $field) {
+            $fieldName = $field->field;
+            $fieldValue = $row[$fieldName];
+            if (!isset($fieldValue)) {
+                continue;
+            }
+
+            if (isset($GLOBALS['TCA'][$table]['columns'][$fieldName])) {
+                $label = $this->languageService->sL(BackendUtility::getItemLabel($table, $fieldName));
+                $content = BackendUtility::getProcessedValue($table, $fieldName, $fieldValue, 0, false, false, $row['uid']);
+            } else {
+                $label = $this->languageService->sL($field->getLabel());
+                $content = $fieldValue;
+            }
+            if (!$content && $field->isSkipIfEmpty()) {
+                continue;
+            }
+            $text = $field->isPrefixLabel() ? sprintf('%s: %s', $label, $content) : $content;
+            $action = (new ResultItemAction($table . '_' . $fieldName))
+                ->setLabel($text)
+                ->setIcon($field->icon ? $this->iconFactory->getIcon($field->icon, Icon::SIZE_SMALL) : null);
+            $resultItem->addAction($action);
+        }
+    }
+
+    protected function addNotesToResult(ResultItem $resultItem, Table $searchConfiguration, string $table, array $row): void
+    {
+        $notesField = $GLOBALS['TCA'][$table]['ctrl']['descriptionColumn'] ?? null;
+        if ($notesField && ($row[$notesField] ?? false) && $searchConfiguration->getUseNotesField()) {
+            $content = BackendUtility::getProcessedValue($table, $notesField, $row[$notesField]);
+
+            $action = (new ResultItemAction($table . '_descriptionColumn'))
+                ->setLabel($content)
+                ->setIcon($this->iconFactory->getIcon('actions-notebook', Icon::SIZE_SMALL));
+            $resultItem->addAction($action);
+        }
+    }
+
+    protected function appendIdToTitle(ResultItem $resultItem, int $id): void
+    {
+        $currentTitle = $resultItem->jsonSerialize()['itemTitle'] ?? false;
+        if ($currentTitle) {
+            $resultItem->setItemTitle(sprintf('%s [%s]', $currentTitle, $id));
+        }
+    }
+
 }
